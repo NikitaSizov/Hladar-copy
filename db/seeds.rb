06171f786816511d796ddda5000d766b77be01a6
Igor Onischenko <optimum.flaminis@gmail.com>
2016-07-22 21:26:24 +0300

    seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,23 +11,25 @@ ProdCategory.create(name: "Огнетушители порошковые")
 ProdCategory.create(name: "Огнетушители углекислотные")
 ProdCategory.create(name: "Пожарный инвентарь")
 
-Product.create(name: "ОП-1(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:2)
-Product.create(name: "ОП-2(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:2)
-Product.create(name: "ОП-3(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:2)
-Product.create(name: "ОП-5(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:2)
-Product.create(name: "ОП-8(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:2)
-Product.create(name: "ОП-9(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:2)
+cat = ProdCategory.all
 
-Product.create(name: "ОУ-2", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:3)
-Product.create(name: "ОУ-3", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:3)
-Product.create(name: "ОУ-5", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:3)
-Product.create(name: "ОУ-7", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:3)
+Product.create(name: "ОП-1(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[1].id)
+Product.create(name: "ОП-2(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[1].id)
+Product.create(name: "ОП-3(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[1].id)
+Product.create(name: "ОП-5(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[1].id)
+Product.create(name: "ОП-8(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[1].id)
+Product.create(name: "ОП-9(з)", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[1].id)
 
-Product.create(name: "Шкафы, щиты пожарные", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:4)
-Product.create(name: "Краны пожарные", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:4)
-Product.create(name: "Рукава пожарные", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:4)
-Product.create(name: "Компдектующие к огнетушителям", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:4)
-Product.create(name: "Кронштейны", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:4)
+Product.create(name: "ОУ-2", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[2].id)
+Product.create(name: "ОУ-3", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[2].id)
+Product.create(name: "ОУ-5", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[2].id)
+Product.create(name: "ОУ-7", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[2].id)
+
+Product.create(name: "Шкафы, щиты пожарные", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[3].id)
+Product.create(name: "Краны пожарные", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[3].id)
+Product.create(name: "Рукава пожарные", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[3].id)
+Product.create(name: "Компдектующие к огнетушителям", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[3].id)
+Product.create(name: "Кронштейны", desc: "Some description", img:"main-photo.jpg", price: 10, quantity: 100, props:"some properties", prod_category_id:cat[3].id)
 
 Sertificate.create name:"1", image: "1.png"
 Sertificate.create name:"2", image: "2.png"
@@ -41,18 +43,20 @@ Sertificate.create name:"9", image: "9.png"
 Sertificate.create name:"10", image: "10.png"
 Sertificate.create name:"11", image: "11.png"
 
-AboutMenu.create product_id:1, prod_category_id:2, order:1
-AboutMenu.create product_id:2, prod_category_id:2, order:2
-AboutMenu.create product_id:3, prod_category_id:2, order:3
-AboutMenu.create product_id:4, prod_category_id:2, order:4
-AboutMenu.create product_id:5, prod_category_id:2, order:5
+prods = Product.all
 
-AboutMenu.create product_id:7, prod_category_id:3, order:6
-AboutMenu.create product_id:8, prod_category_id:3, order:7
+AboutMenu.create product_id:prods[0].id, prod_category_id:cat[1].id, order:1
+AboutMenu.create product_id:prods[1].id, prod_category_id:cat[1].id, order:2
+AboutMenu.create product_id:prods[2].id, prod_category_id:cat[1].id, order:3
+AboutMenu.create product_id:prods[3].id, prod_category_id:cat[1].id, order:4
+AboutMenu.create product_id:prods[4].id, prod_category_id:cat[1].id, order:5
 
-AboutMenu.create product_id:11, prod_category_id:0, order:8
-AboutMenu.create product_id:12, prod_category_id:0, order:9
-AboutMenu.create product_id:13, prod_category_id:0, order:10
+AboutMenu.create product_id:prods[6].id, prod_category_id:cat[2].id, order:6
+AboutMenu.create product_id:prods[7].id, prod_category_id:cat[2].id, order:7
+
+AboutMenu.create product_id:prods[10].id, prod_category_id:cat[0].id, order:8
+AboutMenu.create product_id:prods[11].id, prod_category_id:cat[0].id, order:9
+AboutMenu.create product_id:prods[12].id, prod_category_id:cat[0].id, order:10
 
 News.create name:"Новость1", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet, justo eu condimentum mattis, urna urna elementum ex, in mollis ante urna eget tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam ut commodo est, ut posuere neque. Maecenas mollis viverra lacus, non rutrum leo cursus convallis. Nunc ac malesuada quam. Mauris ultricies turpis nec tellus pretium ultrices. Sed sed ligula et felis semper vulputate. Etiam et pharetra orci.", img: "mission.jpg"
 News.create name:"Новость2", desc:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet, justo eu condimentum mattis, urna urna elementum ex, in mollis ante urna eget tortor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam ut commodo est, ut posuere neque. Maecenas mollis viverra lacus, non rutrum leo cursus convallis. Nunc ac malesuada quam. Mauris ultricies turpis nec tellus pretium ultrices. Sed sed ligula et felis semper vulputate. Etiam et pharetra orci.", img: "news2.jpg"
